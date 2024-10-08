97fcdec3c2a8224b4be7e1a61d7a6650224f26ad
Amnzo <salmi.ensa.ilsi@gmail.com>
2024-10-08 18:58:46 +0100

    add date_renvoi2

--- Avoir/settings.py
+++ Avoir/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'Avoir.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-
+#database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
